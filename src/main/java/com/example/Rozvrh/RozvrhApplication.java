8883954eb6e49bdbe02c89107aff3e1d52feb668
Lenovo <abystrican@student.umb.sk>
2022-03-23 12:37:51 +0100

    Pridavam

---
+++ src/main/java/com/example/Rozvrh/RozvrhApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rozvrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RozvrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RozvrhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Rozvrh/RozvrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rozvrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RozvrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
